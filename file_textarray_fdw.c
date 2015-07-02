290fd26c2fb810256e4a5dd4197ab03abe82b00b
Andrew Dunstan <andrew@dunslane.net>
2015-07-01 20:42:00 -0400

    adjust for 9.5

--- file_textarray_fdw.c
+++ file_textarray_fdw.c
@@ -14,8 +14,8 @@
 #include "postgres.h"
 
 /* check that we are compiling for the right postgres version */
-#if PG_VERSION_NUM < 90500
-#error wrong Postgresql version this branch is only for 9.5
+#if PG_VERSION_NUM < 90600
+#error wrong Postgresql version this branch is only for 9.6
 #endif
 
 #include <sys/stat.h>
