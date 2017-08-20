332575ba5536fa8854bf53b54d301488419f3498
Andrew Dunstan <andrew@dunslane.net>
2017-08-20 08:58:38 -0400

    Mark branch for PG 10 only

--- file_textarray_fdw.c
+++ file_textarray_fdw.c
@@ -14,7 +14,7 @@
 #include "postgres.h"
 
 /* check that we are compiling for the right postgres version */
-#if PG_VERSION_NUM < 100000
+#if PG_VERSION_NUM < 100000 || PG_VERSION_NUM >= 110000
 #error wrong Postgresql version this branch is only for 10
 #endif
 
