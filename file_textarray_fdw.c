b14ed6f4d6a4a19bafa30ae936e967b20411f966
Andrew Dunstan <andrew@dunslane.net>
2020-06-08 10:36:07 -0400

    create REL 13 branch

--- file_textarray_fdw.c
+++ file_textarray_fdw.c
@@ -14,8 +14,8 @@
 #include "postgres.h"
 
 /* check that we are compiling for the right postgres version */
-#if PG_VERSION_NUM < 140000
-#error wrong Postgresql version this branch is only for 14
+#if PG_VERSION_NUM < 130000 || PG_VERSION_NUM  >= 140000
+#error wrong Postgresql version this branch is only for 13
 #endif
 
 #include <sys/stat.h>
