af6de07b348252c1560c60a345de92a4463adfe6
Andrew Dunstan <andrew@dunslane.net>
2021-06-28 17:52:35 -0400

    adjust for REL_14_STABLE

--- file_textarray_fdw.c
+++ file_textarray_fdw.c
@@ -14,8 +14,8 @@
 #include "postgres.h"
 
 /* check that we are compiling for the right postgres version */
-#if PG_VERSION_NUM < 140000
-#error wrong Postgresql version this branch is only for 14
+#if PG_VERSION_NUM < 150000
+#error wrong Postgresql version this branch is only for 15
 #endif
 
 #include <sys/stat.h>
