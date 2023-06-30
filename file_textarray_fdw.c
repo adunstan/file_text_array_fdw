b540048ec3045e021500d1b4c02c667db3623a26
Andrew Dunstan <andrew@dunslane.net>
2023-06-30 06:45:47 -0400

    create REL_16_STABLE

--- file_textarray_fdw.c
+++ file_textarray_fdw.c
@@ -14,7 +14,7 @@
 #include "postgres.h"
 
 /* check that we are compiling for the right postgres version */
-#if PG_VERSION_NUM < 160000
+#if PG_VERSION_NUM < 160000 || PG_VERSION_NUM >= 170000
 #error wrong Postgresql version this branch is only for 16
 #endif
 
